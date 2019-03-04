d32f7a159ae11989a3c5dd79ad702a5de7c367ca
Vikas Kumar Jangir <vikas.jangir@inmobi.com>
2019-03-04 13:02:43 +0530

    Reset

--- BrowseMe/BrowseMe/BrowseMeViewController+SearchBarDelegate.swift
+++ BrowseMe/BrowseMe/BrowseMeViewController+SearchBarDelegate.swift
@@ -11,38 +11,38 @@ import UIKit
 
 extension BrowseMeViewController : UISearchBarDelegate {
     
-    
+}
     // Implement UISearchBarDelegate delegate
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-        let searchString = searchBar.text!
-        if searchString.count > 0 {
-            let encodedSeachTerm = searchString.replacingOccurrences(of: " ", with: "+")
-            let webURL = URL(string: "https://duckduckgo.com/?q=\(encodedSeachTerm)&ia=about")
-            webViewApp.load(URLRequest(url: webURL!))
-        }
-        //        var urlString = searchBar.text
-        //
-        //        if (!(urlString!.hasPrefix("http://"))) && !(urlString!.hasPrefix("https://")) {
-        //            urlString = "http://"+(urlString!)
-        //        }
-        //
-        //        if let url = URL(string: urlString!) {
-        //            webViewApp.load(URLRequest(url: url))
-        //        } else {
-        //            print("Error")
-        //        }
-    }
-    // https://duckduckgo.com/?q=vikas&ia=about
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        let searchString = searchBar.text!
-        if searchString.count > 0 {
-            let encodedSeachTerm = searchString.replacingOccurrences(of: " ", with: "+")
-            let webURL = URL(string: "https://duckduckgo.com/?q=\(encodedSeachTerm)&ia=about")
-            webViewApp.load(URLRequest(url: webURL!))
-        }
-        
-    }
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        searchBar.resignFirstResponder()
+//        let searchString = searchBar.text!
+//        if searchString.count > 0 {
+//            let encodedSeachTerm = searchString.replacingOccurrences(of: " ", with: "+")
+//            let webURL = URL(string: "https://duckduckgo.com/?q=\(encodedSeachTerm)&ia=about")
+//            webViewApp.load(URLRequest(url: webURL!))
+//        }
+//        //        var urlString = searchBar.text
+//        //
+//        //        if (!(urlString!.hasPrefix("http://"))) && !(urlString!.hasPrefix("https://")) {
+//        //            urlString = "http://"+(urlString!)
+//        //        }
+//        //
+//        //        if let url = URL(string: urlString!) {
+//        //            webViewApp.load(URLRequest(url: url))
+//        //        } else {
+//        //            print("Error")
+//        //        }
+//    }
+//    // https://duckduckgo.com/?q=vikas&ia=about
+//    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+//        let searchString = searchBar.text!
+//        if searchString.count > 0 {
+//            let encodedSeachTerm = searchString.replacingOccurrences(of: " ", with: "+")
+//            let webURL = URL(string: "https://duckduckgo.com/?q=\(encodedSeachTerm)&ia=about")
+//            webViewApp.load(URLRequest(url: webURL!))
+//        }
+    
+ //   }
     
     
     
@@ -69,4 +69,4 @@ extension BrowseMeViewController : UISearchBarDelegate {
 //        }
 //    }
     
-}
+
--- BrowseMe/BrowseMe/BrowseMeViewController.swift
+++ BrowseMe/BrowseMe/BrowseMeViewController.swift
@@ -11,31 +11,29 @@ import WebKit
 
 class BrowseMeViewController: UIViewController  {
 
-    @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var webViewApp: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        webViewApp.navigationDelegate = self
-        webViewApp.load(URLRequest(url: URL(string: "https://www.google.com")!))
-        webViewApp.allowsBackForwardNavigationGestures = true
+//        webViewApp.navigationDelegate = self
+//        webViewApp.load(URLRequest(url: URL(string: "https://www.google.com")!))
+//        webViewApp.allowsBackForwardNavigationGestures = true
     }
 
-    @IBAction func back(_ sender: Any) {
-        if webViewApp.canGoBack {
-            webViewApp.goBack();
-        }
-    }
-    
-    @IBAction func refresh(_ sender: Any) {
-        webViewApp.reload()
-    }
-    
-    @IBAction func next(_ sender: Any) {
-        if webViewApp.canGoForward {
-            webViewApp.goForward()
-        }
-    }
+//    @IBAction func back(_ sender: Any) {
+//        if webViewApp.canGoBack {
+//            webViewApp.goBack();
+//        }
+//    }
+//
+//    @IBAction func refresh(_ sender: Any) {
+//        webViewApp.reload()
+//    }
+//
+//    @IBAction func next(_ sender: Any) {
+//        if webViewApp.canGoForward {
+//            webViewApp.goForward()
+//        }
+//    }
     
 }
 
